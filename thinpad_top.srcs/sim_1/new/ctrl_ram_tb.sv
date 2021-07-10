b350459f7722e359cd1a1b80c30108a5280f5047
41802099-guorui <3144027763@qq.com>
2021-07-10 22:00:31 +0800

    初步完成ram控制模块及其测试

---
+++ thinpad_top.srcs/sim_1/new/ctrl_ram_tb.sv
@@ -0,0 +1,227 @@
+`timescale  1ns / 1ps
+
+module tb_Control_ram;
+
+// Control_ram Parameters
+parameter PERIOD  = 10;
+
+
+// Control_ram Inputs
+reg   [31:0]  inst_addr                    = 0 ;
+reg   id_read_ram_en                       = 0 ;
+reg   [31:0]  id_read_ram_addr             = 0 ;
+reg   [3:0]  read_be                       = 0 ;
+reg   wb_write_ram_en                      = 0 ;
+reg   [31:0]  wb_write_ram_addr            = 0 ;
+reg   wb_write_ram_data                    = 0 ;
+reg   [3:0]  write_be                      = 0 ;
+
+// Control_ram Outputs
+wire  pc_stall                             ;    
+wire  [31:0]  inst                         ;    
+wire  [31:0]  id_read_ram_data             ;    
+wire  id_stall                             ;    
+wire  base_ram_ce_n                        ;    
+wire  base_ram_oe_n                        ;
+wire  base_ram_we_n                        ;
+wire  [3:0]  base_ram_be_n                 ;
+wire  [19:0]  base_ram_addr                ;
+wire  ext_ram_ce_n                         ;
+wire  ext_ram_oe_n                         ;
+wire  ext_ram_we_n                         ;
+wire  [3:0]  ext_ram_be_n                  ;
+wire  [19:0]  ext_ram_addr                 ;
+
+// Control_ram Bidirs
+wire  [31:0]  base_ram_data                ;
+wire  [31:0]  ext_ram_data                 ;
+
+reg clk = 1;
+
+initial
+begin
+    forever #(PERIOD/2)  clk=~clk;
+end
+
+Control_ram  u_Control_ram (
+    .inst_addr               ( inst_addr          [31:0] ),
+    .id_read_ram_en          ( id_read_ram_en            ),
+    .id_read_ram_addr        ( id_read_ram_addr   [31:0] ),
+    .read_be                 ( read_be            [3:0]  ),
+    .wb_write_ram_en         ( wb_write_ram_en           ),
+    .wb_write_ram_addr       ( wb_write_ram_addr  [31:0] ),
+    .wb_write_ram_data       ( wb_write_ram_data         ),
+    .write_be                ( write_be           [3:0]  ),
+
+    .pc_stall                ( pc_stall                  ),
+    .inst                    ( inst               [31:0] ),
+    .id_read_ram_data        ( id_read_ram_data   [31:0] ),
+    .id_stall                ( id_stall                  ),
+    .base_ram_ce_n           ( base_ram_ce_n             ),
+    .base_ram_oe_n           ( base_ram_oe_n             ),
+    .base_ram_we_n           ( base_ram_we_n             ),
+    .base_ram_be_n           ( base_ram_be_n      [3:0]  ),
+    .base_ram_addr           ( base_ram_addr      [19:0] ),
+    .ext_ram_ce_n            ( ext_ram_ce_n              ),
+    .ext_ram_oe_n            ( ext_ram_oe_n              ),
+    .ext_ram_we_n            ( ext_ram_we_n              ),
+    .ext_ram_be_n            ( ext_ram_be_n       [3:0]  ),
+    .ext_ram_addr            ( ext_ram_addr       [19:0] ),
+
+    .base_ram_data           ( base_ram_data      [31:0] ),
+    .ext_ram_data            ( ext_ram_data       [31:0] )
+);
+
+// BaseRAM 仿真模型
+sram_model base1(/*autoinst*/
+            .DataIO(base_ram_data[15:0]),
+            .Address(base_ram_addr[19:0]),
+            .OE_n(base_ram_oe_n),
+            .CE_n(base_ram_ce_n),
+            .WE_n(base_ram_we_n),
+            .LB_n(base_ram_be_n[0]),
+            .UB_n(base_ram_be_n[1]));
+sram_model base2(/*autoinst*/
+            .DataIO(base_ram_data[31:16]),
+            .Address(base_ram_addr[19:0]),
+            .OE_n(base_ram_oe_n),
+            .CE_n(base_ram_ce_n),
+            .WE_n(base_ram_we_n),
+            .LB_n(base_ram_be_n[2]),
+            .UB_n(base_ram_be_n[3]));
+// ExtRAM 仿真模型
+sram_model ext1(/*autoinst*/
+            .DataIO(ext_ram_data[15:0]),
+            .Address(ext_ram_addr[19:0]),
+            .OE_n(ext_ram_oe_n),
+            .CE_n(ext_ram_ce_n),
+            .WE_n(ext_ram_we_n),
+            .LB_n(ext_ram_be_n[0]),
+            .UB_n(ext_ram_be_n[1]));
+sram_model ext2(/*autoinst*/
+            .DataIO(ext_ram_data[31:16]),
+            .Address(ext_ram_addr[19:0]),
+            .OE_n(ext_ram_oe_n),
+            .CE_n(ext_ram_ce_n),
+            .WE_n(ext_ram_we_n),
+            .LB_n(ext_ram_be_n[2]),
+            .UB_n(ext_ram_be_n[3]));
+
+parameter BASE_RAM_INIT_FILE = "E:\\aGr_CPU\\lab1.bin"; //BaseRAM初始化文件，请修改为实际的绝对路径
+parameter EXT_RAM_INIT_FILE = "E:\\aGr_CPU\\ext_ram.bin";    //ExtRAM初始化文件，请修改为实际的绝对路径
+
+// 从文件加载 BaseRAM
+initial begin 
+    reg [31:0] tmp_array[0:1048575];
+    integer n_File_ID, n_Init_Size;
+    n_File_ID = $fopen(BASE_RAM_INIT_FILE, "rb");
+    if(!n_File_ID)begin 
+        n_Init_Size = 0;
+        $display("Failed to open BaseRAM init file");
+    end else begin
+        n_Init_Size = $fread(tmp_array, n_File_ID);
+        n_Init_Size /= 4;
+        $fclose(n_File_ID);
+    end
+    $display("BaseRAM Init Size(words): %d",n_Init_Size);
+    for (integer i = 0; i < n_Init_Size; i++) begin
+        base1.mem_array0[i] = tmp_array[i][24+:8];
+        base1.mem_array1[i] = tmp_array[i][16+:8];
+        base2.mem_array0[i] = tmp_array[i][8+:8];
+        base2.mem_array1[i] = tmp_array[i][0+:8];
+    end
+end
+
+// 从文件加载 ExtRAM
+initial begin 
+    reg [31:0] tmp_array[0:1048575];
+    integer n_File_ID, n_Init_Size;
+    n_File_ID = $fopen(EXT_RAM_INIT_FILE, "rb");
+    if(!n_File_ID)begin 
+        n_Init_Size = 0;
+        $display("Failed to open ExtRAM init file");
+    end else begin
+        n_Init_Size = $fread(tmp_array, n_File_ID);
+        n_Init_Size /= 4;
+        $fclose(n_File_ID);
+    end
+    $display("ExtRAM Init Size(words): %d",n_Init_Size);
+    for (integer i = 0; i < n_Init_Size; i++) begin
+        ext1.mem_array0[i] = tmp_array[i][24+:8];
+        ext1.mem_array1[i] = tmp_array[i][16+:8];
+        ext2.mem_array0[i] = tmp_array[i][8+:8];
+        ext2.mem_array1[i] = tmp_array[i][0+:8];
+    end
+end
+
+initial
+begin
+    # (PERIOD*2) inst_addr = 32'h80000000;
+    id_read_ram_en = 0;
+    id_read_ram_addr = 32'h80000004;
+    read_be = 4'b0000;
+    wb_write_ram_en = 0;
+    wb_write_ram_data = 32'h12345678;
+    wb_write_ram_addr = 32'h80003000;
+    write_be = 4'b0000;
+
+    # (PERIOD*2) inst_addr = 32'h80000004;
+    id_read_ram_en = 0;
+    id_read_ram_addr = 32'h80400010;
+    read_be = 4'b0000;
+    wb_write_ram_en = 0;
+    wb_write_ram_data = 32'h12345678;
+    wb_write_ram_addr = 32'h80403000;
+    write_be = 4'b0000;
+
+    # (PERIOD*2) inst_addr = 32'h80000008;
+    id_read_ram_en = 0;
+    id_read_ram_addr = 32'h80400004;
+    read_be = 4'b0000;
+    wb_write_ram_en = 0;
+    wb_write_ram_data = 32'h12345678;
+    wb_write_ram_addr = 32'h80401000;
+    write_be = 4'b0000;
+
+    # (PERIOD*2) inst_addr = 32'h800000c;
+    id_read_ram_en = 0;
+    id_read_ram_addr = 32'h80002000;
+    read_be = 4'b0000;
+    wb_write_ram_en = 0;
+    wb_write_ram_data = 32'h12345678;
+    wb_write_ram_addr = 32'h80403000;
+    write_be = 4'b0000;
+
+    # (PERIOD*2) inst_addr = 32'h80000030;
+    id_read_ram_en = 1;
+    id_read_ram_addr = 32'h80002000;
+    read_be = 4'b0000;
+    wb_write_ram_en = 0;
+    wb_write_ram_data = 32'h12345678;
+    wb_write_ram_addr = 32'h80003000;
+    write_be = 4'b0000;
+
+    # (PERIOD*2) inst_addr = 32'h80000004;
+    id_read_ram_en = 0;
+    id_read_ram_addr = 32'h80002000;
+    read_be = 4'b0000;
+    wb_write_ram_en = 0;
+    wb_write_ram_data = 32'h12345678;
+    wb_write_ram_addr = 32'h80403000;
+    write_be = 4'b0000;
+
+    # (PERIOD*2) inst_addr = 32'h80000020;
+    id_read_ram_en = 0;
+    id_read_ram_addr = 32'h80002000;
+    read_be = 4'b0000;
+    wb_write_ram_en = 0;
+    wb_write_ram_data = 32'h12345678;
+    wb_write_ram_addr = 32'h80403000;
+    write_be = 4'b0000;
+    $finish;
+end
+
+
+
+
+endmodule
---
+++ thinpad_top.srcs/sources_1/new/gr_cpu/Control_ram.v
@@ -0,0 +1,91 @@
+module Control_ram (
+    input wire [31:0] inst_addr,
+    output wire pc_stall,
+    output wire [31:0] inst, 
+
+    input wire id_read_ram_en,
+    input wire [31:0] id_read_ram_addr,
+    input wire [3:0] read_be,
+    output wire [31:0] id_read_ram_data, 
+    output wire id_stall,  
+
+    input wire wb_write_ram_en,
+    input wire [31:0] wb_write_ram_addr,
+    input wire wb_write_ram_data,
+    input wire [3:0] write_be,
+
+    output wire base_ram_ce_n,
+    output wire base_ram_oe_n,
+    output wire base_ram_we_n,
+    output wire [3:0] base_ram_be_n,
+    output wire [19:0] base_ram_addr,
+    inout wire [31:0] base_ram_data,
+
+    output wire ext_ram_ce_n,
+    output wire ext_ram_oe_n,
+    output wire ext_ram_we_n,
+    output wire [3:0] ext_ram_be_n,
+    output wire [19:0] ext_ram_addr,
+    inout wire [31:0] ext_ram_data
+
+
+);
+    wire inst_ram;
+    wire read_ram;
+    wire write_ram;
+    assign inst_ram = inst_addr[22];   // 0 为baseram
+    assign read_ram = id_read_ram_addr[22];
+    assign write_ram = wb_write_ram_addr[22];
+
+
+    assign ext_ram_ce_n = 0;
+    assign base_ram_ce_n = 0;
+
+    wire pc_stall_t;
+    wire id_stall_t;
+    assign pc_stall = pc_stall_t;
+    assign id_stall = id_stall_t;
+
+    // gen pc_stall signal
+    assign pc_stall_t = (id_read_ram_en & ~(inst_ram ^ read_ram) |
+                        wb_write_ram_en & ~(inst_ram ^ write_ram) |
+                        id_stall_t) ? 1:0;
+
+    // gen id_stall signal
+    assign id_stall_t = (id_read_ram_en & 
+                        wb_write_ram_en & 
+                        ~(read_ram ^ write_ram)) ? 1:0;
+    // fecth inst
+    assign inst = pc_stall_t ? 32'd0 : (~inst_ram ? base_ram_data : ext_ram_data);
+    // assign inst = inst_ram ? base_ram_data : ext_ram_data;
+    // gen base ram signals
+    assign base_ram_oe_n = ~(~inst_ram & ~pc_stall_t | 
+                             ~read_ram & id_read_ram_en & ~id_stall_t) ;
+    assign base_ram_we_n = ~(wb_write_ram_en & ~write_ram) ;
+
+    assign base_ram_be_n = wb_write_ram_en & ~write_ram ? write_be : 
+                           id_read_ram_en & ~read_ram   ? read_be : 4'd0;
+
+    assign base_ram_addr = wb_write_ram_en & ~write_ram ? wb_write_ram_addr[21:2] : 
+                           id_read_ram_en & ~read_ram   ? id_read_ram_addr[21:2] : 
+                           inst_addr[21:2];
+    assign base_ram_data = wb_write_ram_en & ~write_ram ? wb_write_ram_data : 32'bz;
+
+    // gen ext_ram_signals
+    assign ext_ram_oe_n = ~(inst_ram & ~pc_stall_t | 
+                             read_ram & id_read_ram_en & ~id_stall_t) ;
+    assign ext_ram_we_n = ~(wb_write_ram_en & write_ram) ;
+
+    assign ext_ram_be_n = wb_write_ram_en & write_ram ? write_be : 
+                           id_read_ram_en & read_ram   ? read_be : 4'd0;
+
+    assign ext_ram_addr = wb_write_ram_en & write_ram ? wb_write_ram_addr[21:2] : 
+                           id_read_ram_en & read_ram   ? id_read_ram_addr[21:2] : 
+                           inst_addr[21:2];
+    assign ext_ram_data = wb_write_ram_en & write_ram ? wb_write_ram_data : 32'bz;
+
+    // gen id signals
+    assign id_read_ram_data = id_read_ram_en & ~id_stall_t ? 
+                              (read_ram ? ext_ram_data : base_ram_data) : 32'bz;
+
+endmodule
