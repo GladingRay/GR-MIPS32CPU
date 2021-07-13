ea33ffe77f6afa51e48206e4cc990800ebd30c51
41802099-guorui <3144027763@qq.com>
2021-07-13 11:19:23 +0800

    增加if段流水寄存器

--- thinpad_top.srcs/sources_1/new/gr_cpu/inst_fetch/Inst_fetch.v
+++ thinpad_top.srcs/sources_1/new/gr_cpu/inst_fetch/Inst_fetch.v
@@ -4,7 +4,12 @@ module Inst_fetch (
     input wire stall_pc,
     input wire is_branch,
     input wire [31:0] target_pc,
-    output wire [31:0] current_pc
+
+    output wire [31:0] current_pc,
+
+    input wire [31:0] inst_in,
+    output reg [31:0] inst_out_id,
+    output reg [31:0] pc_out_id
 );
     wire [31:0] new_pc;
     
@@ -24,4 +29,12 @@ module Inst_fetch (
 
         .current_pc              ( current_pc   )
     );
+
+    always @(posedge clk) begin
+        if(~stall_id) begin
+            inst_out <= inst_in;
+            pc_out <= current_pc;
+        end
+        
+    end
 endmodule
