f046552a320b6780f6a2494bdc6ce426682eb5b2
41802099-guorui <3144027763@qq.com>
2021-07-15 22:48:55 +0800

    增加mipsc3指令集指令的宏定义

--- thinpad_top.srcs/sources_1/new/gr_cpu/inst_define.v
+++ thinpad_top.srcs/sources_1/new/gr_cpu/inst_define.v
@@ -4,13 +4,32 @@
 // op_code
 `define ORI     6'b001101
 `define LUI     6'b001111
-`define SPECIAL    6'b000000
+`define SPECIAL 6'b000000
+`define ADDI    6'b001100
+`define MUL     6'b011100
+`define ANDI    6'b001100
+`define ADDIU   6'b001001
 `define BNE     6'b000101
+`define BEQ     6'b000100
+`define BLEZ    6'b000110 
+`define BGTZ    6'b000111
+`define J       6'b000010
+`define JAL     6'b000011
 `define LW      6'b100011
 `define SW      6'b101011
+`define LB      6'b100000
+`define SB      6'b101000 
+
 
 // funct
-`define ADDU_FUNCT 6'b100001
+`define ADDU_FUNCT  6'b100001
+`define AND_FUNCT   6'b100100
+`define JR_FUNCT    6'b001000 
+`define OR_FUNCT    6'b100101
+`define SRLV_FUNCT  6'b000110
+`define SLL_FUNCT   6'b000000 
+`define SRL_FUNCT   6'b000010 
+`define XOR_FUNCT   6'b100110 
 
 // alu_op
 `define OP_NOP 4'd0
@@ -18,5 +37,7 @@
 `define OP_LUI 4'd2
 `define OP_SUBU 4'd3
 `define OP_OR 4'd4
+`define OP_AND 4'd5
+`define OP_XOR 4'd6
 
 `endif 
